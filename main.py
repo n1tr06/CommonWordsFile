a7cbac52a543c2ede887cb0752a04c8e12b30b6c
n1tr06 <eitangil1016@gmail.com>
2024-06-16 20:52:09 +0300

    made dict

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ with open(file_path, 'r') as file:
     file_content = file.read()
 
 pattern = r'[^a-zA-Z\s]'
-clean_text = re.sub(pattern , '', file_content)
+clean_text = re.sub(pattern , ' ', file_content)
+
+words = clean_text.split()
+
+print(words)
+words_dict = {}
+for i in range(len(words)):
+    if words[i] in words_dict:
+        words_dict[words[i]] += 1
+    else:
+        words_dict[words[i]] = 0
 
 
-    
+print (words_dict)
+
+
 
 
 
