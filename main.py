fd86a816757955bb551bf1876f9ec6a95a9eb15f
n1tr06 <eitangil1016@gmail.com>
2024-06-17 13:54:01 +0300

    ;)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
+import sys 
 
-file_path = 'test.txt'
+file_path = r'C:\Users\eitan\Desktop\Moadon\Git Repositories\CommonWordsFile\test.txt'
 
 with open(file_path, 'r') as file:
     file_content = file.read()
@@ -16,14 +17,9 @@ for i in range(len(words)):
     if words[i] in words_dict:
         words_dict[words[i]] += 1
     else:
-        words_dict[words[i]] = 0
+        words_dict[words[i]] = 1
 
-
-print (words_dict)
-
-
-
-
-
-
-
+for i in range(int(sys.argv[1])):
+    max_key = max(words_dict, key=lambda k: words_dict[k])
+    print(f"{(i+1)}. {max_key} {words_dict[max_key]} times")
+    words_dict[max_key] = 0
